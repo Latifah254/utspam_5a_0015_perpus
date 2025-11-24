1d57bf08b5a5253b5f01966a18f1626351bd542f
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2025-11-24 18:19:28 +0700

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'views/register.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Perpustakaan Digital',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+        ),
+      ),
+      home: const RegisterScreen(),
+    );
+  }
+}
